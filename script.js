256ad7e125b4b607fd0ba607c568f9a3320bd120
alexmiziuk <muzikbasss@gmail.com>
2023-04-29 08:47:19 +0200

    feat: add code to js that is responsible for entering characters from non-physical keyboard buttons

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let keys = document.querySelectorAll('.keys');
+let backspace_key = document.querySelector(".backspace_key")
+let tab = document.querySelector(".tab_key")
 let spaceKey = document.querySelector('.space_key');
 let shift_left = document.querySelector('.shift_left');
 let shift_right = document.querySelector('.shift_right');
 let caps_lock_key = document.querySelector(".caps_lock_key");
+let enter_key = document.querySelector(".enter_key");
+let ctrl_left = document.querySelector(".ctrl_left");
+let win_key = document.querySelector('.win_key');
+let alt_left = document.querySelector('.alt_left');
+let fn_key = document.querySelector(".fn_key")
 let toggle_circle = document.querySelector(".toggle_circle");
 let night_mode = document.querySelector(".night_mode");
 let body = document.querySelector("body");
@@ -100,3 +107,53 @@ colors_input.addEventListener("input", function () {
 })
 
 
+ for (let i = 0; i < keys.length; i++) {
+	keys[i].addEventListener('click', function() {
+		let key = keys[i].getAttribute('keyname');
+		if (caps_lock_key.getAttribute('keyname') === keys[i].getAttribute('keyname') || shift_left.getAttribute('keyname') === keys[i].getAttribute('keyname') || tab.getAttribute('keyname') === keys[i].getAttribute('keyname') || ctrl_left.getAttribute('keyname') === keys[i].getAttribute('keyname') || win_key.getAttribute('keyname') === keys[i].getAttribute('keyname') || alt_left.getAttribute('keyname') === keys[i].getAttribute('keyname') || fn_key.getAttribute('keyname') === keys[i].getAttribute('keyname') || enter_key.getAttribute('keyname') === keys[i].getAttribute('keyname') || backspace_key.getAttribute('keyname') === keys[i].getAttribute('keyname')) {
+			text_input.value += ""
+		} else {
+			text_input.value += key;
+			
+		}
+		
+	});
+}
+
+// add event listener to space key
+spaceKey.addEventListener('click', function() {
+	text_input.value += ' ';
+});
+
+// add event listener to caps lock key
+caps_lock_key.addEventListener('click', function() {
+	// toggle caps lock functionality
+	// ...
+	caps_lock_key.classList.toggle("active");
+
+for (let i = 0; i < keys.length; i++) {
+let key = keys[i];
+let letter = key.getAttribute('keyname');
+let lowerCaseLetter = key.getAttribute('lowerCaseName');
+	if (caps_lock_key.classList.contains('active')) {
+		key.innerText = letter.toUpperCase();
+		key.setAttribute('keyname', letter.toUpperCase());
+		key.setAttribute('lowerCaseName', lowerCaseLetter);
+  } else {
+		key.innerText = letter.toLowerCase();
+		key.setAttribute('keyname', letter.toLowerCase());
+		key.setAttribute('lowerCaseName', lowerCaseLetter);
+  }
+}
+});
+
+// add event listener to shift keys
+shift_left.addEventListener('click', function() {
+	// activate shift functionality for next character/symbol pressed
+	// ...
+});
+
+shift_right.addEventListener('click', function() {
+	// activate shift functionality for next character/symbol pressed
+	// ...
+});
